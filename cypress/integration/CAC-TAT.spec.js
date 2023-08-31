80099168f23c4f91b9eaf2e1ad2ed1ad0d1fcf97
Igorsousa1397 <igor.oa.sousa@gmail.com>
2023-08-30 22:29:10 -0300

    Atualização/ ultima

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -147,8 +147,51 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#privacy a').invoke('removeAttr', 'target').click()
     })
 
-    it('testa a página da política de privacidade de forma independente',function(){
+    it('testa a página da política de privacidade de forma independente',function() {
         cy.get('#privacy a').invoke('removeAttr', 'target').click()
         cy.contains('CAC TAT - Política de privacidade').should('be.visible')
     })
-  })
+
+    it('exibe e esconde as mensagens de sucesso e erro usando o .invoke', function() {
+      cy.get('.success').should('not.be.visible').invoke('show').should('be.visible')
+        .and('contain', 'Mensagem enviada com sucesso.').invoke('hide').should('not.be.visible')
+      cy.get('.error').should('not.be.visible').invoke('show').should('be.visible')
+        .and('contain', 'Valide os campos obrigatórios!').invoke('hide').should('not.be.visible')
+    })
+    
+    it('preenche a area de texto usando o comando invoke', function() {
+      //.repeat serve para criar textos longo de forma ctrl+v
+      const textoLongo = Cypress._.repeat ('0123456789', 20)
+      cy.get('#open-text-area').invoke('val', textoLongo).should('have.value', textoLongo)
+    })
+
+    it('faz uma requisição HTTP', function() {
+      cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function(response){
+        const { status, statusText, body } = response
+        expect(status).to.equal(200)
+        expect(statusText).to.equal('OK')
+        expect(body).to.include('CAC TAT')
+      })
+
+    })
+    it.only('encontrar o gato',function(){
+      cy.get('#cat').should('not.be.visible').invoke('show').should('be.visible')
+    })
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ })
